c7beddf6be8b4d17ef7e8b2aee5e9f75190a2c61
Andre <SoftwareTech@gmail.com>
2023-12-11 00:15:47 -0300

    Subindo Testes

---
+++ Modulo01/Modulo01/Program.cs
@@ -0,0 +1,47 @@
+﻿using Modulo01;
+
+OperacoesCalculadora operacoes = new OperacoesCalculadora();
+
+Console.WriteLine("### Exemplos Testes Unitarios ###");
+Console.WriteLine("Escolha a operação:");
+Console.WriteLine("1 - Projeto Calculadora e teste");
+Console.WriteLine("2 - Projeto Banco e teste");
+
+int escolha = Convert.ToInt32(Console.ReadLine());
+
+switch (escolha)
+{
+    case 1:
+        Console.WriteLine("### Exemplos Testes Unitarios CALCULADORA ###");
+
+        Console.WriteLine("Deseja realizar cálculos ou testar a calculadora?");
+        string opcao = Console.ReadLine().ToLower();
+
+        switch (opcao)
+        {
+            case "testar":
+                operacoes.RealizarTeste();
+                break;
+
+            case "calcular":
+                operacoes.RealizarCalculo();
+                break;
+
+            default:
+                Console.WriteLine("Opção inválida. Por favor, escolha 'Testar' ou 'Calcular'.");
+                break;
+        }
+
+        break;
+
+    case 2:
+        Console.WriteLine("### Exemplos Testes Unitarios BANCO ###");
+        // Adicione a lógica para o projeto Banco, se necessário
+        break;
+
+    default:
+        Console.WriteLine("Opção inválida. Por favor, escolha 'Testar' ou 'Calcular'.");
+        break;
+}
+
+Console.ReadLine();
